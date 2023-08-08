9b2527c9bca8df8d3e9be8706f5d6acf63045e02
Amirali Amanipour <arshia.ala80@gmail.com>
2023-08-08 12:26:06 +0330

    initial folder

---
+++ components/layout/Layout.js
@@ -0,0 +1,55 @@
+import { BiSolidHand } from "react-icons/bi";
+import Link from 'next/link';
+import {VscListSelection} from 'react-icons/vsc';
+import {BiMessageSquareAdd} from 'react-icons/bi';
+import {RxDashboard} from 'react-icons/rx';
+
+function Layout({children}) {
+  return (
+    <div>
+        <header className='bg-blue-700 pb-16 pt-4 text-white flex justify-between items-center'>
+            <p className="font-semibold text-lg mb-8">Botostart Todo App</p>
+        </header>
+
+        <div className="flex justify-between"> 
+
+            <aside className="bg-white  p-2 
+            rounded-r-lg z-10 w-2/12 h-screen relative bottom-16">
+
+                <p className="flex items-center ">Welcome <BiSolidHand /></p>
+                <ul className="my-4 flex flex-col gap-y-4">
+                    <li className=" gap-x-2 flex items-center text-gray-500">
+                        <VscListSelection />
+                        <Link href="/">Todos</Link>
+                    </li>
+
+                    
+                    <li className=" gap-x-2 flex items-center text-gray-500 ">
+                        <BiMessageSquareAdd />
+                        <Link href="/">Add Todo</Link>
+                    </li>
+
+                    
+                    <li className="gap-x-2 flex items-center text-gray-500">
+                        <RxDashboard />
+                        <Link href="/">Profile</Link>
+                    </li>
+                </ul>
+            </aside>
+
+            <section className="w-10/12  bg-zinc-300 ">
+                {children}
+                awdkawlad
+            </section>
+
+        </div>
+
+        {/* <footer>
+            awd
+        </footer> */}
+
+    </div>
+  )
+}
+
+export default Layout
---
+++ models/User.js
@@ -0,0 +1,30 @@
+import { Schema, model, models } from "mongoose";
+
+
+const userSchema= new Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+    },
+    lastName:{
+        type:String
+    }
+    ,
+    createdAt:{
+        type:Date,
+        default:()=> Date.now(),
+        immutalbe:true,
+    }
+    
+});
+
+const User= models.User || model("User", userSchema);
+
+export default User;
