fae6f0097b5b57b89e6501de092a143bbd48c4ed
Amirali Amanipour <arshia.ala80@gmail.com>
2023-08-20 13:02:05 +0330

    signin page

--- components/templates/SignInPage.js
+++ components/templates/SignInPage.js
@@ -23,7 +23,7 @@ function SignInPage() {
             email, password, redirect:false
         });
         console.log(res);
-        if(!res.error) router.replace('/')
+        if(res.status===200) router.replace('/')
 
     }
 
